78f116824f595b214aa2addd104fb668773b6e64
Patrick Tran (Pat-GPT) <67556421+patricktran1@users.noreply.github.com>
2026-07-10 21:14:15 -0700

    Add parent dashboard route

---
+++ adrianos-play-starter/app/parent/page.tsx
@@ -0,0 +1,6 @@
+import ParentDashboard from "@/components/ParentDashboard";
+import { games } from "@/lib/generated-games";
+
+export default function ParentPage() {
+  return <ParentDashboard games={games} />;
+}
