5d78e5197f0298ff48085b788c873b4434620616
Patrick Tran (Pat-GPT) <67556421+patricktran1@users.noreply.github.com>
2026-07-12 20:12:46 -0700

    Mount Power Locker companions across games

--- adrianos-play-starter/app/games/layout.tsx
+++ adrianos-play-starter/app/games/layout.tsx
@@ -4,16 +4,19 @@ import GameFeelShell from "@/components/GameFeelShell";
 import GameFlowDirector from "@/components/GameFlowDirector";
 import GamePowerLoop from "@/components/GamePowerLoop";
 import GameStartDirector from "@/components/GameStartDirector";
+import PowerLockerCompanion from "@/components/PowerLockerCompanion";
 import "./adventure-chain.css";
 import "./game-feel.css";
 import "./game-feel-stability.css";
 import "./game-flow.css";
 import "./game-power-loop.css";
 import "./game-start.css";
+import "./power-locker.css";
 
 export default function GamesLayout({ children }: { children: ReactNode }) {
   return (
     <GameFeelShell>
+      <PowerLockerCompanion />
       <GamePowerLoop />
       <GameStartDirector />
       <GameFlowDirector />
